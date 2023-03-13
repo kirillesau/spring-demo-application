0ad459d0961ebafb9790f97f82834665ac07b5bc
kirill.esau <kirill.esau@adesso.de>
2023-03-13 18:51:12 +0100

    books can be initialized

--- src/main/java/de/kirill/springdemoapplication/DemoController.java
+++ src/main/java/de/kirill/springdemoapplication/DemoController.java
@@ -1,14 +1,26 @@
 package de.kirill.springdemoapplication;
 
+import de.kirill.springdemoapplication.book.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class DemoController {
 
-    @GetMapping(path = "/ping")
+    private final BookService bookService;
+
+    @GetMapping(path = "/demo/ping")
     ResponseEntity<String> ping() {
         return ResponseEntity.ok("pong");
     }
+
+    @GetMapping(path = "/demo/init")
+    ResponseEntity<Void> initData() {
+        bookService.initBooks();
+        return ResponseEntity.ok().build();
+    }
+
 }
