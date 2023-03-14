dbcfd28a8290456ce5c36e96bd525e33ac2c31f8
kirill.esau <kirill.esau@adesso.de>
2023-03-14 14:50:20 +0100

    added host and port

--- src/main/java/de/kirill/springdemoapplication/DemoController.java
+++ src/main/java/de/kirill/springdemoapplication/DemoController.java
@@ -2,19 +2,25 @@ package de.kirill.springdemoapplication;
 
 import de.kirill.springdemoapplication.book.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequiredArgsConstructor
 public class DemoController {
 
     private final BookService bookService;
+    private final Environment environment;
 
     @GetMapping(path = "/demo/ping")
-    ResponseEntity<String> ping() {
-        return ResponseEntity.ok("pong");
+    ResponseEntity<String> ping() throws UnknownHostException {
+        return ResponseEntity.ok("pong from Host: " + InetAddress.getLocalHost().getHostName() + " and Port: " + environment.getProperty("local.server.port"));
     }
 
     @GetMapping(path = "/demo/init")
