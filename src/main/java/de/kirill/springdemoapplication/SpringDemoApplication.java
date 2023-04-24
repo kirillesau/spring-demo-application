ff018e4aba1329207487bd298f519057d5ab1b41
kirill.esau <kirill.esau@adesso.de>
2023-04-24 20:08:19 +0200

    old os

--- src/main/java/de/kirill/springdemoapplication/SpringDemoApplication.java
+++ src/main/java/de/kirill/springdemoapplication/SpringDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
-        // Test Workflow 5
+        // Test Workflow 3
         SpringApplication.run(SpringDemoApplication.class, args);
     }
 
